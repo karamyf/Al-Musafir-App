24ebdc7002b0bcd20773d681a3a9bc010c640b2d
karamyf <karam_youssef@emsi-edu.ma>
2023-04-04 09:06:14 +0000

    user avatar

--- app/src/main/java/easy/tuto/myquizapplication/MainActivity.java
+++ app/src/main/java/easy/tuto/myquizapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package easy.tuto.myquizapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.app.AlertDialog;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -114,6 +118,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
         finish();
     }
+
 
 
 
--- app/src/main/java/easy/tuto/myquizapplication/Register.java
+++ app/src/main/java/easy/tuto/myquizapplication/Register.java
@@ -1,8 +1,12 @@
 package easy.tuto.myquizapplication;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -24,6 +28,24 @@ public class Register extends AppCompatActivity {
     Button btnRegister;
 
     FirebaseAuth mAuth;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    // Inside your activity or fragment
+    public void selectImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    // Handle the result of the intent
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            // Do something with the imageUri, like upload it to Firebase Storage
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
